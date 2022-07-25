c98c746a27b752146f569d0864445bd9c7deeb6f
Bruno <bruno_zangi@hotmail.com>
2022-07-25 04:17:02 -0300

    Add How 2 comment in JS

--- basics/aula7_consolelog/index.js
+++ basics/aula7_consolelog/index.js
@@ -3,7 +3,13 @@ console.log("Brunão");
 console.log(`Brunão`); // String
 
 console.log(12, 12.12) // Number
-
+/** 
+  Bloco comentado
+  Bloco comentado
+  Bloco comentado
+  Bloco comentado
+*/
+// Comentando em JS
 const text1 = 'Meu nome é '
 const text2 = '. Estou aprendendo JavaScript às'
 const text3 = ' da madrugada.'
