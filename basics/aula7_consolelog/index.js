8d68612d2096df83c4bc5df7e530d0fd9707c105
Bruno <bruno_zangi@hotmail.com>
2022-07-25 04:07:13 -0300

    Add console log class

---
+++ basics/aula7_consolelog/index.js
@@ -0,0 +1,11 @@
+console.log('Brunão');
+console.log("Brunão");
+console.log(`Brunão`); // String
+
+console.log(12, 12.12) // Number
+
+const text1 = 'Meu nome é '
+const text2 = '. Estou aprendendo JavaScript às'
+const text3 = ' da madrugada.'
+
+console.log(text1, 'Bruno, o fudido', text2, 4, text3)
