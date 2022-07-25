2d2c4a45e8b4a41dd1f2f12df9ccb7ef178ee1aa
Bruno <bruno_zangi@hotmail.com>
2022-07-25 04:53:32 -0300

    Creating variables with 'let'

---
+++ basics/aula13_let/index.js
@@ -0,0 +1,13 @@
+let name = 'Bruno'; // variável inicializada
+// ou
+let otherName; // variável não inicializada
+console.log(otherName);
+otherName = 'Ninguém';
+console.log(otherName);
+
+console.log(name, 'was born in 1997');
+console.log(name, 'grew up and got bigger.');
+name = 'Brunão';
+console.log('Now his friends call him', name);
+console.log('In 2022', name, 'met Camila.');
+console.log('then they never met again');
