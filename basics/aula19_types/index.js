da7a3daa24678b0e63158e55ca626b2c94181c31
Bruno <bruno_zangi@hotmail.com>
2022-07-25 06:12:50 -0300

    Add JS primitive types

---
+++ basics/aula19_types/index.js
@@ -0,0 +1,7 @@
+// symbol
+let ablublue; // undefined -> não aponta para local algum na memória (eu não posso escolher deixar a variável assim)
+const creoQueNo = null; // null -> não aponta para local algum na memória
+const name = 'Bruno'; // string
+const age = 25; //number
+const height = 1.85; //number
+const boolean = true; //boolean -> true or false
