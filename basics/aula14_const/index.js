151118305c79b4a43e4a26713b2fbd6a27753b23
Bruno <bruno_zangi@hotmail.com>
2022-07-25 05:13:27 -0300

    Creating constants

---
+++ basics/aula14_const/index.js
@@ -0,0 +1,15 @@
+/**
+* Não utilizar palavras reservadas
+* Utilizar nomes significativos
+* Não começar o nome com um número
+* Não pode conter espaços ou traços
+* camelCase e case sensitive
+* O valor de uma constante não pode ser modificado depois de inicializada
+* não utilizar var
+*/
+
+const firstNumber = 15.5;
+console.log(firstNumber)
+const secondNumber = .5;
+const sum = firstNumber + secondNumber;
+console.log(sum)
