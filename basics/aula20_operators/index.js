26c45b61f8208d00e22e4d1ace20e396e3b17f95
Bruno <bruno_zangi@hotmail.com>
2022-07-25 06:55:16 -0300

    Add JS operators

---
+++ basics/aula20_operators/index.js
@@ -0,0 +1,15 @@
+/** Em ordem de precedência
+ * ()
+ * ** Potenciação
+ * * Multiplicação
+ * / Divisão
+ * % Resto da divisão | módulo
+ * + Adição/Concatenação
+ * - Subtração
+ * 
+ * 
+ * ++ operador de incremento (pode ser inserido antes ou depois da variável)
+ * -- operador de decremento (pode ser inserido antes ou depois da variável)
+ * 
+ * += | -= | *= | /= | **= operadores de atribuição
+ */
